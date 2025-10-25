5317fa8282878317c1a3e1f736df8dd55e336ee8
sampanna <swarnim@Sampanna-MacBook-Air.local>
2025-10-25 03:47:04 -0400

    refractor: implement modular design

--- src/learn.js
+++ src/learn.js
@@ -4,24 +4,38 @@ const hobbybox1 = document.getElementById("hobby1");
 const hobbybox2 = document.getElementById("hobby2");
 const button = document.getElementById("submit");
 const cows = document.getElementsByName("cow")
-const output = document.getElementById("output here");
+const output = document.getElementById("outputhere");
 
+const cow_opinion_generator = ()=>
+{
+  let cowanswer = "don't know";
+  for (const radio of cows)
+  {
+    if (radio.checked){
+      if (radio.value == "True")
+      {
+        cowanswer = "like"
+      }
+      else
+      {
+        cowanswer = "don't like"
+      }
+      break
+    }
+  }
+  return cowanswer
+};
 button.addEventListener("click", () => 
 {
   const text = namebox.value;
   const age = agebox.value;
   const hobby1 = hobbybox1.value;
   const hobby2 = hobbybox2.value;
-  let cowanswer = "ok"
-  for (const radio of cows){
-    if (radio.checked) {
-        cowanswer = radio.value;
-    }
-  }
+  const cowanswer = cow_opinion_generator();
   output.innerHTML = `
     <strong>Hello, ${text}!</strong><br>
     You are ${age} years old.<br>
-    You ${cowanswer.toLowerCase()} like cows.<br>
+    You ${cowanswer} cows.<br>
     Your hobbies are: ${hobby1} and ${hobby2}.
   `;
 });
